57bb2868a79a1a586fb6924bf2204b52ad0c1345
Pavel Mishanin <pavelmishanin@mail.ru>
2021-11-20 18:37:10 +0700

    Create Dependency in ViewController and Injection them

--- DependencyInjectionForBeginner/ViewController.swift
+++ DependencyInjectionForBeginner/ViewController.swift
@@ -9,8 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var viewModel: ViewModel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let storage = Storage()
+        let dataManager = DataManager(storage: storage)
+        viewModel = ViewModel(dataManager: dataManager)
+        viewModel?.save()
+    }
+    
+}
+
+class ViewModel {
+    private let dataManager: DataManager
+    
+    init(dataManager: DataManager) {
+        self.dataManager = dataManager
+    }
+    
+    func save() {
+        dataManager.save()
     }
 }
 
+class DataManager {
+    private let storage: Storage
+    
+    init(storage: Storage) {
+        self.storage = storage
+    }
+    
+    func save() {
+        storage.save()
+    }
+}
+
+class Storage {
+
+    func save() {
+        print("Save")
+    }
+}
