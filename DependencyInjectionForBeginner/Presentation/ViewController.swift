11ded8dc3dcf717e59fec12e176e66ace81ecb48
Pavel Mishanin <pavelmishanin@mail.ru>
2021-11-22 14:02:53 +0700

    Create Activity Indicator View Create Label

--- DependencyInjectionForBeginner/Presentation/ViewController.swift
+++ DependencyInjectionForBeginner/Presentation/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     
     private let label = UILabel()
-    
+    private let activityIndicatorView = UIActivityIndicatorView()
     private let viewModel = DependencyFactory().provideViewModel()
     
     override func viewDidLoad() {
@@ -20,23 +20,37 @@ class ViewController: UIViewController {
         view.backgroundColor = .green
         viewModel.save()
         showLoader()
+        initLabel()
+        
+    }
+    
+    private func activityIndicator() {
+        activityIndicatorView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        activityIndicatorView.color = .red
+        activityIndicatorView.center = view.center
+        activityIndicatorView.startAnimating()
+        view.addSubview(activityIndicatorView)
+    }
+    
+    private func initLabel() {
+        label.frame = CGRect(x: 0, y: 0, width: 300, height: 100)
+        label.center = view.center
+        view.addSubview(label)
         
         viewModel.fetchMovies(){ movies in
+            self.label.text = "\(movies)"
             print(movies)
             self.hideLoader()
         }
     }
     
-    private func initLabel() {
-        label.frame = CGRect(x: 0, y: 0, width: 200, height: 100)
-    }
-    
     private func showLoader() {
+        activityIndicator()
         
     }
     
     private func hideLoader() {
-        
+        activityIndicatorView.stopAnimating()
     }
     
 }
